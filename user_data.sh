9d705f3fefc2485a0c9752b74b3812d338c08b1c
mateus <mhenrique4060@gmail.com>
2024-04-01 09:15:37 -0300

    add user_data script

---
+++ user_data.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+yum update -y
+
+# INSTALANDO E CONFIGURANDO PARA QUE O DOCKER SEJA INICIADO JUNTO AO SISTEMA
+yum install docker -y
+systemctl start docker.service
+systemctl enable docker.service
+
+# INSTALANDO O DOCKER-COMPOSE
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compos
+chmod +x /usr/bin/docker-compose
+
+# INSTALANDO NFS
+yum install nfs-utils -y
+systemctl start nfs-utils.service
+systemctl enable nfs-utils.service
+mkdir -p /efs
+
+# MONTANDO AUTOMATICAMENTE O EFS
+echo "fs-05aa9716a33be6d92.efs.us-east-1.amazonaws.com:/ /efs nfs defaults 0 0" >> /etc/fstab
+mount -a
+
+# CRIANDO O DIRETORIO DO WP
+mkdir -p /efs/wordpress
+
+# CONFIG O DOCKER-COMPOSE
+services:
+  wordpress:
+    image: wordpress:latest
+    volumes:
+      - /efs/wordpress:/var/www/html
+    ports:
+      - 80:80
+    restart: always
+    environment:
+      WORDPRESS_DB_HOST: # [RDS ENDPOINT]
+      WORDPRESS_DB_USER: # [RDS USER]
+      WORDPRESS_DB_PASSWORD: # [USER PASSWORD]
+      WORDPRESS_DB_NAME: # [RDS INITIAL NAME]
+
+docker-compose -f /efs/docker-compose.yml up -d
