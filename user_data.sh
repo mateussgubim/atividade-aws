8811b24004b60b59a23d7eba7d9223fbc906ada6
mateus <mhenrique4060@gmail.com>
2024-04-01 10:08:46 -0300

    typo fix

--- user_data.sh
+++ user_data.sh
@@ -8,7 +8,7 @@ systemctl start docker.service
 systemctl enable docker.service
 
 # INSTALANDO O DOCKER-COMPOSE
-curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compos
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 
 # INSTALANDO NFS
